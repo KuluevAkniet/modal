cd86084ea5e653dba7c4ceff4b36c22747a9e6c3
AizadaK <kasenova.aizada@gmail.com>
2022-01-22 15:56:29 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let modalbtn = document.getElementById('hide');
+let modal = document.getElementById('modal')
+console.log(modal);
+
+
+modalbtn.addEventListener('click',() => {
+    modal.style.display = 'block';
+})
